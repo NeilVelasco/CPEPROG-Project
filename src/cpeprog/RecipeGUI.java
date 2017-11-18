c0f734a88d04acd02998f62306ae7e0f62cf6728
ChristophJohnEric <ChristophJohnEric@192.168.1.6>
2017-11-18 15:35:53 +0800

    change variable names

--- src/cpeprog/RecipeGUI.java
+++ src/cpeprog/RecipeGUI.java
@@ -48,7 +48,7 @@ public class RecipeGUI extends javax.swing.JFrame {
         jPanel7 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        procedureTextArea = new javax.swing.JTextArea();
         jPanel2 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
@@ -56,7 +56,7 @@ public class RecipeGUI extends javax.swing.JFrame {
         jPanel5 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        ingredientsTextArea = new javax.swing.JTextArea();
 
         jPopupMenu1.setMaximumSize(new java.awt.Dimension(20, 50));
         jPopupMenu1.setMinimumSize(new java.awt.Dimension(100, 100));
@@ -129,11 +129,11 @@ public class RecipeGUI extends javax.swing.JFrame {
         jLabel6.setFont(new java.awt.Font("Avalon", 1, 14)); // NOI18N
         jLabel6.setText("Procedure");
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jTextArea2.setText("ENTER PROCEDURE HERE");
-        jScrollPane2.setViewportView(jTextArea2);
-        jTextArea2.setEditable(false);
+        procedureTextArea.setColumns(20);
+        procedureTextArea.setRows(5);
+        procedureTextArea.setText("ENTER PROCEDURE HERE");
+        jScrollPane2.setViewportView(procedureTextArea);
+        procedureTextArea.setEditable(false);
 
         javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
         jPanel7.setLayout(jPanel7Layout);
@@ -208,11 +208,11 @@ public class RecipeGUI extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Avalon", 1, 14)); // NOI18N
         jLabel4.setText("Ingredients");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jTextArea1.setText("ENTER INGREDIENTS HERE");
-        jScrollPane1.setViewportView(jTextArea1);
-        jTextArea1.setEditable(false);
+        ingredientsTextArea.setColumns(20);
+        ingredientsTextArea.setRows(5);
+        ingredientsTextArea.setText("ENTER INGREDIENTS HERE");
+        jScrollPane1.setViewportView(ingredientsTextArea);
+        ingredientsTextArea.setEditable(false);
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
@@ -308,13 +308,11 @@ public class RecipeGUI extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(searchButton)
-                            .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(menuButton)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(searchButton)
+                        .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(menuButton))
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -392,6 +390,7 @@ public class RecipeGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea ingredientsTextArea;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private ImageIcon imageIcon;
@@ -416,9 +415,8 @@ public class RecipeGUI extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSpinner jSpinner1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
     private javax.swing.JButton menuButton;
+    private javax.swing.JTextArea procedureTextArea;
     private javax.swing.JButton searchButton;
     private javax.swing.JTextField searchField;
     // End of variables declaration//GEN-END:variables
